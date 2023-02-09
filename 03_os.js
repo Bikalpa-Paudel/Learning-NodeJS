39b882a464922c9b16907d455564f0092fe9b6be
Bikalpa <bikalpapaudel53@gmail.com>
2023-02-09 13:26:20 +0545

    learning os module

--- 03_os.js
+++ 03_os.js
@@ -1,4 +1,6 @@
 /* os is the built in module provided by node.js 
     help us to use various bulit-in methods related to operating system */
     
-    
+const os = require('os')
+
+console.log(os.cpus())
