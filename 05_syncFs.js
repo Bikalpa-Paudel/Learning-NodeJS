3b0cb2c96f8cfefcb05263ea2b2d174c00a8e1b4
Bikalpa <bikalpapaudel53@gmail.com>
2023-02-14 12:39:43 +0545

    learning more about Fs module

--- 05_syncFs.js
+++ 05_syncFs.js
@@ -1,10 +1,17 @@
 //The fs readFileSync() method is an inbuilt application programming interface of fs module which is used to read the file and return its content. 
 
 
-const {readFileSync} = require('fs')
+const {readFileSync, writeFileSync, writeFile} = require('fs')
 
 const firstText = readFileSync('./test/first.txt', "utf-8")
 const secondText = readFileSync('./test/second.txt', "utf-8")
 
-console.log(firstText)
-console.log(secondText)
+// console.log(firstText)
+// console.log(secondText)
+
+
+//the fs writeFileSync() method do write on mention file path. And if the is no any txt file exist on that path, it creates one
+
+writeFileSync(
+    './test/third.txt', `It writes: ${firstText} and ${secondText}`
+)
